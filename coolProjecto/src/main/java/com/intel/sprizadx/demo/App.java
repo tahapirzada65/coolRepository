b1d22c4cb2ab6bca59fdef80e41277a00fe0bf05
Pirzada, SyedX <syedx.pirzada@intel.com>
2016-04-21 15:30:18 -0700

    Made changes in App.java file

--- coolProjecto/src/main/java/com/intel/sprizadx/demo/App.java
+++ coolProjecto/src/main/java/com/intel/sprizadx/demo/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String name = "Taha"
+        System.out.println( name +" says Hello World!!" );
     }
 }
